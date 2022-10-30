492770e3a474a31ede9b6c6ab9f1af6946b9c4c9
ravindaa <ravindaa@oregonstate.edu>
2022-10-30 12:53:21 +0100

    Removed live, kept examples

--- example_1.py
+++ example_1.py
@@ -1,53 +1,54 @@
 import uuid
 from datetime import datetime
 
+# Keep track of pets in an animal shelter
+
+# Super Class
+# Parent Class
 class Pet:
     def __init__(self, name, species):
         self.name = name
-        self.serial_number = uuid.uuid4().hex
-        self.date_of_entry = datetime.today().strftime('%Y-%m-%d')
         self.species = species
+        # Automatically generate alphanumeric serial code
+        self.serial_number = uuid.uuid4()
+        self.date_of_entry = datetime.today().strftime('%Y-%m-%d')
         self.medical_conditions = []
     
     def add_medical_condition(self, medical_condition):
         self.medical_conditions.append(medical_condition)
         return self.medical_conditions
     
-    def remove_medical_condition(self, medical_condition):
-        self.medical_conditions.remove(medical_condition)
-        return self.medical_conditions
-    
-    def change_name(self, new_name):
-        self.name = new_name
+    def get_serial_number(self):
+        return self.serial_number
     
     def get_diet(self):
         return ['rice']
     
-    # Static Function (used as a tool or utility)
+    # Static Function
     def get_list_of_species():
         species = ["tiger", "dog", "cat", "octopus", "giraffe"]
         return species
-    
-
+# Sub Class
+# Child Class   
 class Tiger(Pet):
-    def __init__(self, region, name, species):
-        super.__init__(name, species)
-        self.region = region
-        self.handler = ''
+    def __init__(self, name, type_of_tiger, volume_of_meat):
+        species = "tiger"
+        super().__init__(name, species)
+        self.type_of_tiger = type_of_tiger
+        self.volume_of_meat = volume_of_meat
     
-    def set_handler(self, handler_name):
-        self.handler = handler_name
-        return self.handler
-
-tiger_1 = Tiger('Siberia')
-print(tiger_1.name)
-print(tiger_1.region)
-tiger_1.add_medical_condition('cold')
-tiger_1.set_handler('Amith')
-
-
-pet_1 = Pet("Alberto", "tiger")
-pet_2 = Pet("Jimmy", "giraffe")
-pets = [pet_1, pet_2]
+    # Function Overloading
+    def get_diet(self, age):
+        if age > 2:
+            return ['rice', 'meat']
+        else:
+            return ['milk']
+    
+tiger_1 = Tiger("Eddie", "Siberia", "20")
+print("Species:", tiger_1.species)
+print("Type of Tiger", tiger_1.type_of_tiger)
+print("Serial Number", tiger_1.serial_number)
+tiger_1.add_medical_condition('covid')
+print("Diet: ", tiger_1.get_diet(1))
     
 
